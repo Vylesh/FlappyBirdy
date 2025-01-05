94c14222dfc224be8d73c5fb37c1ddd52d7f5ddc
Vylesh <19240001475@ogrenci.ege.edu.tr>
2025-01-05 18:11:39 +0300

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [SerializeField] private GameObject GameOverCanvas;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        Time.timeScale = 1f;
+    }
+
+    public void GameOver()
+    {
+        GameOverCanvas.SetActive(true);
+        
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
---
+++ Assets/Scripts/ScoreIncrease.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class ScoreIncrease : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Score.instance.UpdateScore();
+        }
+    }
+}
